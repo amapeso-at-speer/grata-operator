1ea6c7d42516e3b5617b1820260f92ec8d18c52b
amapeso-at-speer <angelica.mapeso@speer.io>
2024-07-05 11:28:35 -0400

    convert bluetooth lock data to json

--- android/app/src/main/java/com/grata_operator/AlfredLibraryModule.java
+++ android/app/src/main/java/com/grata_operator/AlfredLibraryModule.java
@@ -17,6 +17,12 @@ import com.alfred.library.AlfredLibrary;
 import com.facebook.react.bridge.WritableMap;
 import com.facebook.react.modules.core.DeviceEventManagerModule;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class AlfredLibraryModule extends ReactContextBaseJavaModule implements ILockScanner.IListener {
@@ -80,13 +86,39 @@ public class AlfredLibraryModule extends ReactContextBaseJavaModule implements I
 
     @Override
     public void onScanSuccess(List<AlfredBleDevice> list) {
+        JSONArray devices = new JSONArray();
+
         if (!list.isEmpty()) {
             Log.d("AlfredLibraryModule", "Devices found: " + list.toString());
+
+            for (int i = 0; i < list.size(); i++) {
+                JSONObject device = new JSONObject();
+                try {
+                    AlfredBleDevice alfredDevice = list.get(i);
+                    device.put("name", alfredDevice.getName());
+                    device.put("masterID", alfredDevice.getMasterID());
+                    device.put("isPairable", alfredDevice.isPairable());
+                    devices.put(device);
+                } catch (JSONException e) {
+                    Log.d("AlfredLibraryModule", "Error while converting device to JSON: " + e.getMessage());
+                }
+            }
         } else {
             Log.d("AlfredLibraryModule", "No devices found " + list.toString());
         }
 
-        context.getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class).emit("onDevicesSearch", list.toString());
+        // Test data
+        JSONObject test = new JSONObject();
+        try {
+            test.put("name", "Alfred Lock 1");
+            test.put("masterID", "1234567");
+            test.put("isPairable", true);
+            devices.put(test);
+        } catch (JSONException e) {
+            throw new RuntimeException(e);
+        }
+
+        context.getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class).emit("onDevicesSearch", devices.toString());
     }
 
     @Override
